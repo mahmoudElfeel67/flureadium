73243a93b7c79a28d145bb740678939e4f26976e
Daniel Freiling <ddfreiling@gmail.com>
2025-09-08 15:57:00 +0200

    chore: remove Nota x-properties

--- flutter_readium_platform_interface/lib/src/shared/publication/x_periodical_type.dart
+++
@@ -1,8 +0,0 @@
-// Known PeriodicalType values, which can be used to match against.
-class XPeriodicalType {
-  static const associationPaper = 'AssociationPaper';
-  static const localBlindPaper = 'LocalPaperForBlind';
-  static const localPaper = 'LocalPaper';
-  static const magazine = 'Magazine';
-  static const paper = 'Paper';
-}
