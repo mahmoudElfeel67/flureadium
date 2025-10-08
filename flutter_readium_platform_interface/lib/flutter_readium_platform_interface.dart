e7ef2adf8f319650cceef9d839c2ef4833dbcb1d
Daniel Freiling <ddfreiling@gmail.com>
2025-10-08 14:01:19 +0200

    chor: add missing extension

--- flutter_readium_platform_interface/lib/flutter_readium_platform_interface.dart
+++ flutter_readium_platform_interface/lib/flutter_readium_platform_interface.dart
@@ -14,6 +14,7 @@ import 'src/reader/index.dart';
 import 'src/shared/index.dart';
 
 export 'src/exceptions/index.dart';
+export 'src/extensions/index.dart';
 export 'src/reader/index.dart';
 export 'src/shared/index.dart';
 export 'src/utils/index.dart';
