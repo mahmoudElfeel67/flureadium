d5d79e7cc4a428212c5098655fc09af5fa340f6a
SifAa <43337067+SifAa@users.noreply.github.com>
2025-10-24 13:16:05 +0200

    feat: add control panel info type to TTSPreferences

--- flutter_readium_platform_interface/lib/src/reader/reader_tts_preferences.dart
+++ flutter_readium_platform_interface/lib/src/reader/reader_tts_preferences.dart
@@ -1,20 +1,25 @@
+import '../../flutter_readium_platform_interface.dart';
+
 class TTSPreferences {
   TTSPreferences({
     this.speed,
     this.pitch,
     this.voiceIdentifier,
     this.languageOverride,
+    this.controlPanelInfoType,
   });
 
   double? speed;
   double? pitch;
   String? voiceIdentifier;
   String? languageOverride;
+  ControlPanelInfoType? controlPanelInfoType;
 
   Map<String, dynamic> toMap() => {
         'speed': speed,
         'pitch': pitch,
         'voiceIdentifier': voiceIdentifier,
         'languageOverride': languageOverride,
+        'controlPanelInfoType': controlPanelInfoType?.toString().split('.').last,
       };
 }
