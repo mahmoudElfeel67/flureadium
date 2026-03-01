29560b968462d901127e7ea1662f4159999facd2
Mike Mulev <m.mulev@gmail.com>
2026-03-01 11:35:21 +0100

    chore: remove BLoC example app code and simplify dependencies
    Delete pages/, state/, utils/, widgets/, extensions/ from example/lib.
    Strip flutter_bloc, hydrated_bloc, path_provider and other unneeded
    deps from example/pubspec.yaml; update lock file accordingly.

--- flureadium/example/lib/widgets/index.dart
+++
@@ -1,7 +0,0 @@
-export 'list_item.widget.dart';
-export 'player_controls.widget.dart';
-export 'reader.widget.dart';
-export 'text_settings.widget.dart';
-export 'theme_selector.widget.dart';
-export 'timebased.state.widget.dart';
-export 'tts_settings.widget.dart';
